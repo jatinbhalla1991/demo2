3cec7e60a9efafd3b25d8a2589a78fdd8fd36d12
jatinbhalla <jatinbhalla18.jb@gmail.com>
2025-06-18 13:38:16 +0530

    backend

--- provider.tf
+++ provider.tf
@@ -2,7 +2,13 @@ provider "aws" {
   region = "us-west-1"
   #alias = "aws-us-west1"
 }
-
+terraform {
+  backend "s3" {
+    bucket = "terraformstatefilejb"
+    key = "env/dev/terraform.tfstate"
+    region = "us-west-1"
+  }
+}
 # provider "aws" {
 #   region = "us-west-2"
 #   alias = "aws-us-west2"
